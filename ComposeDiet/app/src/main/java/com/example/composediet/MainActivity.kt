d2b83e5b3574fa7474e59c9ebd68c33a1de107f1
Derzhavin <derzhavind@bk.ru>
2021-03-25 16:14:06 +0300

    added missed files

--- ComposeDiet/app/src/main/java/com/example/composediet/MainActivity.kt
+++ ComposeDiet/app/src/main/java/com/example/composediet/MainActivity.kt
@@ -3,35 +3,22 @@ package com.example.composediet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.composediet.ui.theme.ComposeDietTheme
 
 class MainActivity : ComponentActivity() {
+    val foodViewModel by viewModels<FoodViewModel>()
+    val profileViewModel by viewModels<ProfileViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App {
-                Navigation()
+            ComposeDietTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    MainView(foodViewModel,profileViewModel)
+                }
             }
         }
-    }
-}
-
-@Composable
-fun App(content: @Composable () -> Unit) {
-    ComposeDietTheme {
-        Surface(color = MaterialTheme.colors.background) {
-            content()
-        }
-    }
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    App {
-        Navigation()
     }
 }
