980037e12a5085f9db1aa68c0a5f7a20fd7f8127
AlexoAleksei <xanlexan100@gmail.com>
2021-03-26 16:31:08 +0300

    created water section

--- ComposeDiet/app/src/main/java/com/example/composediet/MainActivity.kt
+++ ComposeDiet/app/src/main/java/com/example/composediet/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import com.example.composediet.ui.theme.ComposeDietTheme
@@ -11,14 +12,16 @@ import com.example.composediet.ui.theme.ComposeDietTheme
 class MainActivity : ComponentActivity() {
     val foodViewModel by viewModels<FoodViewModel>()
     val profileViewModel by viewModels<ProfileViewModel>()
+    val waterViewModel by viewModels<WaterViewModel>()
 
+    @ExperimentalFoundationApi
     @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeDietTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    MainView(foodViewModel,profileViewModel)
+                    MainView(foodViewModel,profileViewModel, waterViewModel)
                 }
             }
         }
